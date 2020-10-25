2b5389a706b330f27b29175b4e1f9e7e9cbbe4f5
tmurphy3 <tim.murphy@uconn.edu>
2020-10-25 15:51:25 -0400

    file structure change

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import Filter from "./Filter";
 import Result from "./Result";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <main>
-      <Switch>
-        <Route path="/" component={Filter} exact />
-        <Route path="/result" component={Result} />
-      </Switch>
-      <div className="links">
-        <Link to="/">Home </Link>
-      </div>
-    </main>
+    <div>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route path="/" component={Filter} exact />
+          <Route path="/result" component={Result} />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
