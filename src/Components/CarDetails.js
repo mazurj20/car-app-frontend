ea07c44852604bb8d18911962294ffa94e8fc119
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-11-03 00:46:17 -0500

    working on car details component

--- src/Components/CarDetails.js
+++ src/Components/CarDetails.js
@@ -1,26 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import axios from '../axios'
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "../Styles/CarDetails.css";
 
-function CarDetails({match}) {
-    const [car, setCar] = useState(null)
+function CarDetails({ match }) {
+  const [car, setCar] = useState(null);
 
-    useEffect(() => {
-        axios.get(`/details/${match.params._id}`).then((res) => setCar(res.data));
-      }, []);
-      
-      return (
-        <div>
-            {car && (
-                <div className="cardetail__container">
-                    <img src={`${car[0].image_url}`} />
-                    <p>{car[0].state}</p>
-                    <p>{car[0].manufacturer}</p>
-                    <p>{car[0].model}</p>
-                    <p>{car[0].condition}</p>
-                    <p>{car[0].size}</p>
-                </div>)}
+  useEffect(() => {
+    axios.get(`/details/${match.params._id}`).then((res) => setCar(res.data));
+  }, []);
+
+  return (
+    <div>
+      {car && (
+        <div className="cardetail__container">
+          <img src={`${car[0].image_url}`} />
+          <h1>
+            {car[0].year} {car[0].manufacturer} {car[0].model}
+          </h1>
+          <p>
+            {car[0].odometer === ""
+              ? "mileage unkown"
+              : `${car[0].odometer} miles`}
+          </p>
+          <h1>${car[0].price}</h1>
+          <p>{car[0].state}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default CarDetails
+export default CarDetails;
